ad8ddbeee28fa942a71a9c3ce18e5f8764fe99a9
rohankota <rohankota@u.northwestern.edu>
2025-11-04 19:48:40 -0600

    adding sensor calibration data

--- annotate_data.py
+++ annotate_data.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv('sensor_calibration_data/annotations/probe_data.csv')
+df['x_px'] = 10
+df['y_px'] = 10
 
+df.to_csv("sensor_calibration_data/annotations/annotations.csv", index=False)
 print(df)
